de22449c01771a428ece94f67b1045359bb9f473
Vladykovsky <SuGer1628@gmail.com>
2021-12-20 11:01:03 +0300

    add last lab

---
+++ oop-class-book/BookListService.Tests/BookListService.Tests.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using BookClass;
+using NUnit.Framework;
+
+namespace BookListService.Tests
+{
+    [TestFixture]
+    public class BookServiceTests
+    {
+        [TestCase("Erich Maria Remarque", "Three comrades", "Little, Brown and Company")]
+        public void BookService_Add_Book(string author, string title, string publisher)
+        {
+            BookListService bookListService = new BookListService();
+            Book book = new Book(author, title, publisher);
+
+            bookListService.Add(book);
+            Assert.IsFalse(bookListService.isEmpty());
+        }
+
+        [TestCase("Erich Maria Remarque", "Three comrades", "Little, Brown and Company")]
+        public void BookService_Add_Same_Book(string author, string title, string publisher)
+        {
+            BookListService bookListService = new BookListService();
+            Book book = new Book(author, title, publisher);
+
+            bookListService.Add(book);
+            Assert.Throws<Exception>(() => bookListService.Add(book), "This book is already exist");
+        }
+
+        [TestCase("Erich Maria Remarque", "Three comrades", "Little, Brown and Company")]
+        public void BookService_Remove_Book(string author, string title, string publisher)
+        {
+            BookListService bookListService = new BookListService();
+            Book book = new Book(author, title, publisher);
+
+            bookListService.Add(book);
+            bookListService.Remove(book);
+            Assert.IsTrue(bookListService.isEmpty());
+        }
+
+        [TestCase("Erich Maria Remarque", "Three comrades", "Little, Brown and Company")]
+        public void BookService_Remove_Unexisting_Book(string author, string title, string publisher)
+        {
+            BookListService bookListService = new BookListService();
+            Book book = new Book(author, title, publisher);
+            bookListService.Add(new Book(string.Empty, string.Empty, string.Empty));
+
+            Assert.Throws<Exception>(() => bookListService.Remove(book), "Failed to remove the book");
+        }
+
+        [TestCase("Erich Maria Remarque")]
+        public void BookService_FindByAuthor(string author)
+        {
+            BookListService bookListService = new BookListService();
+
+            Book book1 = new Book(string.Empty, string.Empty, "Little, Brown and Company");
+            Book book2 = new Book(author, string.Empty, string.Empty);
+            Book book3 = new Book(string.Empty, "Three comrades", string.Empty);
+            Book book4 = new Book(string.Empty, string.Empty, string.Empty);
+            Book book5 = new Book(author, "Three comrades", string.Empty);
+
+            bookListService.Add(book1);
+            bookListService.Add(book2);
+            bookListService.Add(book3);
+            bookListService.Add(book4);
+            bookListService.Add(book5);
+            
+            List<Book> list = bookListService.FindBy(book => book.Author.Equals(author));
+
+            Assert.IsTrue(list.Contains(book2) && list.Contains(book5) && list.Count == 2);
+        }
+
+        [TestCase("Three comrades")]
+        public void BookService_FindByTitle(string title)
+        {
+            BookListService bookListService = new BookListService();
+
+            Book book1 = new Book(string.Empty, string.Empty, "Little, Brown and Company");
+            Book book2 = new Book("Erich Maria Remarque", string.Empty, string.Empty);
+            Book book3 = new Book(string.Empty, title, string.Empty);
+            Book book4 = new Book(string.Empty, string.Empty, string.Empty);
+            Book book5 = new Book("Erich Maria Remarque", title, string.Empty);
+
+            
+            bookListService.Add(book1);
+            bookListService.Add(book2);
+            bookListService.Add(book3);
+            bookListService.Add(book4);
+            bookListService.Add(book5);
+            
+            List<Book> list = bookListService.FindBy(book => book.Title.Equals(title));
+
+            Assert.IsTrue(list.Contains(book3) && list.Contains(book5) && list.Count == 2);
+        }
+
+        [TestCase("Little, Brown and Company")]
+        public void BookService_FindByPublisher(string publisher)
+        {
+            BookListService bookListService = new BookListService();
+
+            Book book1 = new Book(string.Empty, string.Empty, publisher);
+            Book book2 = new Book("Erich Maria Remarque", string.Empty, string.Empty);
+            Book book3 = new Book(string.Empty, "Three comrades", string.Empty);
+            Book book4 = new Book(string.Empty, string.Empty, string.Empty);
+            Book book5 = new Book("Erich Maria Remarque", "Three comrades", string.Empty);
+
+            bookListService.Add(book1);
+            bookListService.Add(book2);
+            bookListService.Add(book3);
+            bookListService.Add(book4);
+            bookListService.Add(book5);
+            
+            List<Book> list = bookListService.FindBy(book => book.Publisher.Equals(publisher));
+
+            Assert.IsTrue(list.Contains(book1) && list.Count == 1);
+        }
+
+        [TestCase("C", "A", "B")]
+        public void BookService_GetByAuthor(string author1, string author2, string author3)
+        {
+            BookListService bookListService = new BookListService();
+
+            Book book1 = new Book(author1, string.Empty, string.Empty);
+            Book book2 = new Book(author2, string.Empty, string.Empty);
+            Book book3 = new Book(author3, string.Empty, string.Empty);
+   
+            bookListService.Add(book1);
+            bookListService.Add(book2);
+            bookListService.Add(book3);
+            
+            List<Book> list = bookListService.GetBy(new AuthorComparator());
+
+            Assert.IsTrue(list[0] == book2 && list[1] == book3 && list[2] == book1);
+        }
+
+        [TestCase(300, 40, 230)]
+        public void BookService_GetByPages(int pages1, int pages2, int pages3)
+        {
+            BookListService bookListService = new BookListService();
+
+            Book book1 = new Book("A", string.Empty, string.Empty) { Pages = pages1 };
+            Book book2 = new Book("B", string.Empty, string.Empty) { Pages = pages2 };
+            Book book3 = new Book("C", string.Empty, string.Empty) { Pages = pages3 };
+
+            bookListService.Add(book1);
+            bookListService.Add(book2);
+            bookListService.Add(book3);
+            
+            List<Book> list = bookListService.GetBy(new PagesComparator());
+
+            Assert.IsTrue(list[0] == book2 && list[1] == book3 && list[2] == book1);
+        }
+
+        [TestCase(300, 40, 230, "USD")]
+        public void BookService_GetByPrice(int price1, int price2, int price3, string currency)
+        {
+            BookListService bookListService = new BookListService();
+
+            Book book1 = new Book("A", string.Empty, string.Empty);
+            Book book2 = new Book("B", string.Empty, string.Empty);
+            Book book3 = new Book("C", string.Empty, string.Empty);
+
+            book1.SetPrice(price1, currency);
+            book2.SetPrice(price2, currency);
+            book3.SetPrice(price3, currency);
+
+            bookListService.Add(book1);
+            bookListService.Add(book2);
+            bookListService.Add(book3);
+
+            List<Book> list = bookListService.GetBy(new PriceComparator());
+
+            Assert.IsTrue(list[0] == book2 && list[1] == book3 && list[2] == book1);
+        }
+
+        [Test]
+        public void BookService_Save_Into_BookStorage()
+        {
+            BookListService bookListService = new BookListService();
+
+            Book book1 = new Book("A", string.Empty, string.Empty);
+            Book book2 = new Book("B", string.Empty, string.Empty);
+            Book book3 = new Book("C", string.Empty, string.Empty);
+
+            bookListService.Add(book1);
+            bookListService.Add(book2);
+            bookListService.Add(book3);
+
+            BookStorage bookStorage = new BookStorage();
+
+            bookListService.Save(bookStorage);
+            
+            Assert.IsFalse(bookStorage.isEmpty());
+        }
+    }
+}
---
+++ oop-class-book/BookListService/BookListService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookClass;
+
+namespace BookListService
+{
+    public class BookListService
+    {
+        private Dictionary<int, Book> BookList = new Dictionary<int, Book>();
+
+        public bool isEmpty()
+        {
+            return BookList.Count == 0;
+        }
+
+        public void Add(Book book)
+        {
+            if (BookList.ContainsValue(book))
+            {
+                throw new Exception("This book is already exist");
+            } 
+            BookList.Add(BookList.Count, book);
+        }
+
+        public void Remove(Book book)
+        {
+            if (BookList.Count == 0)
+            {
+                return;
+            } 
+            
+            if (!BookList.ContainsValue(book))
+            {
+                throw new Exception("Failed to remove the book");
+            }
+            
+                
+            foreach (var bookItem in BookList)
+            {
+                if (bookItem.Value == book)
+                {
+                    BookList.Remove(bookItem.Key);
+                    return;
+                }
+            }
+
+        }
+        
+        public List<Book> FindBy(Predicate<Book> predicate)
+        {
+            return BookList.Values.ToList().FindAll(predicate);
+        }
+
+        public List<Book> GetBy(IComparer<Book> comparer)
+        {
+            List<Book> books = BookList.Values.ToList();
+            books.Sort(comparer);
+            return books;
+        }
+
+        public void Load(IBookStorage bookStorage)
+        {
+            var books = bookStorage.GetBooks();
+            int count = 0;
+
+            foreach (var item in books)
+            {
+                BookList.Add(count++, item);
+            }
+        }
+        
+        public void Save(IBookStorage bookStorage) 
+        {
+            List<Book> books = new List<Book>();
+
+            foreach (var bookItem in BookList)
+            {
+                books.Add(bookItem.Value);
+            }
+
+            bookStorage.AddBooks(books);
+        }
+    }
+}
